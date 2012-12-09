7825cf3e4c6a81a462347bc0c921ca98e459168c
Fred Alger <fred@fredalger.net>
2012-12-09 13:02:18 -0500

    Omnibus commit.

---
+++ content/first-post.md
@@ -0,0 +1,12 @@
+Title: My super title
+Date: 2010-12-03 10:20
+Tags: thats, awesome
+Category: yeah
+Slug: my-super-post
+Author: Alexis Metaireau
+Summary: Short version for index and feeds
+Foobar: bazwocket
+
+This is the content of my super blog post.
+
+*booyeah*
---
+++ pelicanconf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"theClubhou.se"
+SITENAME = u"theClubhou.se"
+SITEURL = ''
+
+TIMEZONE = 'America/New_York'
+THEME = 'themes/theclubhouse'
+
+DEFAULT_LANG = 'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pcooo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
